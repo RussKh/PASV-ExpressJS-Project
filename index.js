8a9c907b25c0f8649fcf0a0d24cd2389d78503b6
russkh <kharuba@gmail.com>
2024-03-11 22:41:13 -0600

    installed packages

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("test");
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("test");
+});
+
+app.get("/cards", (req, res) => {
+  res.send([
+    { id: 1, name: "First Card", status: "todo", prioroty: 1 },
+    { id: 2, name: "Second Card", status: "progress", prioroty: 5 },
+  ]);
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
