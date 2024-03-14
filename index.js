847f571b6f16064edcae8898c9211a48ade1b8fd
russkh <kharuba@gmail.com>
2024-03-13 20:01:54 -0600

    Card create

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
 const express = require("express");
+var bodyParser = require("body-parser");
+
 const app = express();
 const PORT = 3000;
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+let cards = [
+  { id: 1, name: "First Card", status: "todo", prioroty: 1 },
+  { id: 2, name: "Second Card", status: "progress", prioroty: 5 },
+];
 
 app.get("/", (req, res) => {
   res.send("test");
 });
 
-app.get("/cards", (req, res) => {
-  res.send([
-    { id: 1, name: "First Card", status: "todo", prioroty: 1 },
-    { id: 2, name: "Second Card", status: "progress", prioroty: 5 },
-  ]);
+app.get("/card", (req, res) => {
+  res.send(cards);
 });
 
+app.delete("/card/:cardId", (req, res) => {
+  const cardId = req.params.cardId;
+
+  cards = cards.filter((el) => el.id != cardId);
+  res.send(cards);
+});
+app.post("/card", (req, res) => {
+  const card = req.body;
+  cards.push({ id: Math.random().toString(), ...card });
+  res.send("Card created");
+});
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
 });
